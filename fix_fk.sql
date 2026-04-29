83170fea36ec0d4abf29ad35b224bc3d9a08f405
wruizz <wruizz@DESKTOP-71B8376>
2026-04-29 15:42:58 -0500

    Clean: Eliminar archivos temporales y scripts de despliegue obsoletos; Actualizar wwwroot con build de Angular

--- fix_fk.sql
+++
@@ -1,6 +0,0 @@
--- Primero ejecuta ESTE query para DESHABILITAR la comprobación de FK
-EXEC sp_msforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT all"
-
--- Luego intenta inserting
--- Luego si quieres vuelve a habilitar
-EXEC sp_msforeachtable "ALTER TABLE ? CHECK CONSTRAINT all"
